69f1543d27c9eccf4894812c4fb2197337a0a3cc
Nicolas Kepper <nicolas.kepper@gmail.com>
2018-02-12 11:07:26 +0100

    Trying to fix everything

--- ChatServiceClient/src/frontend/Client.java
+++ ChatServiceClient/src/frontend/Client.java
@@ -13,26 +13,16 @@ public class Client {
 	private static volatile boolean run = true;
 	static String EXIT_CONNECTION_CODE = "%&/(=)(/&%%ojgoOUHFEXITOihg&%&/()";
 
-	public static void main(String[] args)  {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Auf welcher IP Adresse läuft der Server?: ");
-		String ip = keyboard.nextLine();
-		if(!isIp(ip)) {
-			System.out.println(ip + " ist keine richtige IP-Adresse.");
-			System.exit(-1);
-		}
-		
+	public static void main(String[] args) {
 		System.out.println("Versuche Verbindung mit Server aufzubauen...");
 		
-		try {
-			//Socket socket = new Socket("10.61.14.182", 5555);
-			Socket socket = new Socket(ip, 5555);
+		try (Socket socket = new Socket("10.61.14.182", 5555)){
 			System.out.println("Verbindung aufgebaut!");
 			PrintWriter output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 			Scanner input = new Scanner(new InputStreamReader(socket.getInputStream()));
-		
+			Scanner keyboard = new Scanner(System.in);
+			
 			Thread reciever = new Thread(new Runnable() {
-
 				@Override
 				public void run() {
 					while(run) {
@@ -49,11 +39,9 @@ public class Client {
 				if(System.in.available() != 0) output.println(keyboard.nextLine());
 			}
 			
-			
 			keyboard.close();
 			input.close();
 			output.close();
-			socket.close();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
@@ -61,12 +49,5 @@ public class Client {
 	public static void lock() {
 		run = false;
 	}
-	
-	public static boolean isIp(String check) {
-		boolean _byte = true;
-		String[] split = check.split("\\.");
-		for(String _bytecheck : split)
-			_byte = (Integer.parseInt(_bytecheck) & 0xFFFFFF00) == 0 && _byte;
-		return check.split("\\.").length == 4 && _byte;
-	}
+
 }
