a13182c1a1f37b83cc8967b2bb7fdc16c1ac7d14
Nicolas Kepper <nicolas.kepper@gmail.com>
2018-02-12 10:57:52 +0100

    Added help to all shortcuts Clients now have to specify server ip

--- ChatServiceClient/src/frontend/Client.java
+++ ChatServiceClient/src/frontend/Client.java
@@ -13,16 +13,24 @@ public class Client {
 	private static volatile boolean run = true;
 	static String EXIT_CONNECTION_CODE = "%&/(=)(/&%%ojgoOUHFEXITOihg&%&/()";
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Auf welcher IP Adresse läuft der Server?: ");
+		String ip = keyboard.nextLine();
+		if(!isIp(ip)) {
+			System.out.println(ip + " ist keine richtige IP-Adresse.");
+			System.exit(-1);
+		}
+		
 		System.out.println("Versuche Verbindung mit Server aufzubauen...");
 		
 		try {
-			Socket socket = new Socket("10.61.14.182", 5555);
+			//Socket socket = new Socket("10.61.14.182", 5555);
+			Socket socket = new Socket(ip, 5555);
 			System.out.println("Verbindung aufgebaut!");
 			PrintWriter output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 			Scanner input = new Scanner(new InputStreamReader(socket.getInputStream()));
-			Scanner keyboard = new Scanner(System.in);
-			
+		
 			Thread reciever = new Thread(new Runnable() {
 
 				@Override
@@ -52,5 +60,13 @@ public class Client {
 	}
 	public static void lock() {
 		run = false;
+	}
+	
+	public static boolean isIp(String check) {
+		boolean _byte = true;
+		String[] split = check.split("\\.");
+		for(String _bytecheck : split)
+			_byte = (Integer.parseInt(_bytecheck) & 0xFFFFFF00) == 0 && _byte;
+		return check.split("\\.").length == 4 && _byte;
 	}
 }
