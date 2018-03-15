38a135cb0129bc8c77d33557be3b72cef2f41fb2
Nicolas Kepper <nicolas.kepper@gmail.com>
2018-03-15 14:31:15 +0100

    Round Key Calculation

--- ChatServiceServer/src/encryption/Encryption.java
+++ ChatServiceServer/src/encryption/Encryption.java
@@ -24,9 +24,12 @@ public class Encryption {
 	
 	public static void main(String[] args) {
 		//initializeEncryptionProtocol(null);
+		
 		AESEncrypt(new byte[]{ (byte) 0xa1, 0x34, (byte) 0xb5, 0x51, 0x6e, (byte) 0xf0, 0x31, 0x4a, (byte) 0xb7, 0x2c, 0x46, (byte) 0xe3, 0x51, 0x1b, (byte) 0xca, 0x6c
 				, (byte) 0xa1, 0x34, (byte) 0x00b5, 0x51, 0x6e, (byte) 0xf0, 0x31, 0x4a, (byte) 0xb7, 0x2c, 0x46, (byte) 0xe3, 0x51, 0x1b, (byte) 0xca, 0x6b
-				, (byte) 0xa1, 0x34, (byte) 0x00b5, 0x51, 0x6e, (byte) 0xf0, 0x31, 0x4a, (byte) 0xb7, 0x2c, 0x46, (byte) 0xe3, 0x51, 0x1b, (byte) 0xca, 0x6a}, null);
+				, (byte) 0xa1, 0x34, (byte) 0x00b5, 0x51, 0x6e, (byte) 0xf0, 0x31, 0x4a, (byte) 0xb7, 0x2c, 0x46, (byte) 0xe3, 0x51, 0x1b, (byte) 0xca, 0x6a}, 
+
+				new byte[]{0x2b, 0x28, (byte) 0xab, 0x09, 0x7e, (byte) 0xae, (byte) 0xf7, (byte) 0xcf, 0x15, (byte) 0xd2, 0x15, 0x4f, 0x16, (byte) 0xa6, (byte) 0x88, 0x3c});
 	}
 	
 	private static final int ENCRYPTION_KEY_LENGTH = 512;//0x400;
@@ -39,8 +42,8 @@ public class Encryption {
 			e.printStackTrace();
 		}
 		kpg.initialize(ENCRYPTION_KEY_LENGTH);
-		KeyPair kp = kpg.generateKeyPair();
-		Key publicKey = kp.getPublic();
+		java.security.KeyPair kp = kpg.generateKeyPair();
+		java.security.Key publicKey = kp.getPublic();
 		byte[] ba = publicKey.getEncoded();
 
 		try {
@@ -77,18 +80,30 @@ public class Encryption {
 	
 	public static byte[] AESEncrypt(byte[] ciphertext, byte[] key) {
 		if(ciphertext.length == 0) return null;
-		//printHex(ciphertext);
 		Integer[] block = buildBlock(ciphertext);
-		Block blockclass = new Block(arrayToIndex(15, ciphertext));
-		//printHex(block);
-		blockclass.printBlock();
-		
-		
+		EncryptionBlock blockclass = new EncryptionBlock(arrayToIndex(15, ciphertext));
+		Key blockkey = new Key(key);
+		blockclass.shiftWords();
 		//byte[] encrypted_block = null;
+		blockclass.subBytes();
 		
+		System.out.println("Key");
+		Key round1 = blockkey.nextRoundKey();
+		Key round2 = round1.nextRoundKey();
+		round1.print();
+		System.out.println();
+		round2.print();
 		//byte[] encrypted_message = AESEncrypt(arrayFromIndex(16, ciphertext), key);
 		//return expand_array(encrypted_block, encrypted_message);
 		//return AESEncrypt(arrayFromIndex(16, ciphertext), key);
+		return null;
+	}
+	
+	private static byte[] encryptBlock(Block block, Block key) {
+		
+		
+		
+		
 		return null;
 	}
 
@@ -117,7 +132,7 @@ public class Encryption {
 	
 	private static byte[] arrayToIndex(int endindex, byte[] array) {
 		byte[] newarray = new byte[endindex + 1];
-		for(int i = 0; i < endindex; i++) newarray[i] = array[i];
+		for(int i = 0; i <= endindex; i++) newarray[i] = array[i];
 		return newarray;
 	}
 	
@@ -130,49 +145,188 @@ public class Encryption {
 		return newarray;
 	}
 	
-	private static <E> void printHex(E[] array) {
-		for(int i = 0; i < array.length; i++) {
-			System.out.printf("%08x\n", array[i]);
-		}
-		System.out.println();
-	}
-	
-	private static <E> void printHex(E value) {
-		System.out.printf("%08x\n", value);
-	}
-	
-	private static class Block {
-		byte[] bytes;
+	private static abstract class Block {
+		protected byte[] bytes;
+		
+		private static int[] sbox = new int[]{
+			0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, 
+			0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+			0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+			0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+			0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+			0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+			0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+			0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+			0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+			0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+			0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+			0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+			0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+			0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+			0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+			0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+		};
 		
 		public Block(byte[] bytes) {
 			this.bytes = bytes;
 		}
 		
+		public Block(int[] DWORDS) {
+			bytes = new byte[16];
+			for(int i = 0; i < 4; i++)
+				setDWORD(i, DWORDS[i]);
+		}
+		
+		protected byte[] subBytes(byte[] bytes) {
+			for(int i = 0; i < bytes.length; i++)
+				subByte(bytes, i);
+			return bytes;
+		}
+		
+		public void subBytes() {
+			for(int i = 0; i < bytes.length; i++)
+				subByte(i);
+		}
+		
 		public int getDWORD(int index) {
-			int DWORD = 0x000000;
+			int DWORD = 0x00000000;
 			for(int i = 0; i < 4; i++)
 				DWORD |= (bytes[i + index * 4] << (3 - i) * 8) & masks[i];
 			
 			return DWORD;
 		}
 		
-		private int index(int wordIndex, int byteIndex) {
+		/**
+		 * Get Vertical DWORD
+		 * @param index starts at 0
+		 * @return
+		 */
+		public int getVDWORD(int index) {
+			int DWORD = 0x00000000;
+			for(int i = 0; i < 4; i++)
+				DWORD |= (bytes[i * 4 + index] << (3 - i) * 8) & masks[i];
+			return DWORD;
+		}
+		
+		public void setDWORD(int index, int DWORD) {
+			for(int i = 0; i < 4; i++)
+				bytes[i + index * 4] = (byte) (DWORD >> (( 3 - i) * 8) & 0xFF);
+		}
+		
+		public void setVDWORD(int index, int DWORD) {
+			for(int i = 0; i < 4; i++)
+				bytes[i * 4 + index] = (byte) (DWORD >> ((3 - i) * 8) & 0xFF);
+		}
+		
+		protected int index(int wordIndex, int byteIndex) {
 			return wordIndex * 4 + byteIndex;
 		}
 		
-		public void printBlock() {
+		public void print() {
 			for(int i = 0; i < 4; i++)
 				System.out.printf("%08x\n", getDWORD(i));
 		}
 		
-		public void shiftWord(int i, int bytes) {
-			if(bytes == 0) return;
-			byte help = this.bytes[index(i, 3)];
-			for(int j = 3; j > 0; j--)
-				this.bytes[index(i, j)] = this.bytes[index(i, j - 1)];
-			this.bytes[index(i, 0)] = help;
-			shiftWord(i, bytes - 1);
-			return;
+		public void shiftWord(int index, int bytes) {
+			int DWORD = getDWORD(index);
+			DWORD = OFLeftShift(DWORD, 8 * bytes);
+			setDWORD(index, DWORD);
+		}
+
+		public void subByte(int index) {
+			bytes[index] = (byte) sbox[((int) bytes[index]) & 0xff];
+		}
+		
+		protected void subByte(byte[] bytes, int index) {
+			bytes[index] = (byte) sbox[((int) bytes[index]) & 0xff];
+		}
+		
+		/**
+		 * Shifts a DWORD "bits" bits to the left moving the overflowing MSB to LSB
+		 * @param DWORD
+		 * @param bits
+		 * @return
+		 */
+		protected int OFLeftShift(int DWORD, int bits) {
+			return (DWORD << bits) | (DWORD >>> 32 - bits);
+		}
+		
+		protected int OFRightShift(int DWORD, int bits) {
+			return (DWORD >>> bits) | (DWORD << 32 - bits);
+		}
+		
+		@Deprecated
+		private byte OFLeftShift(byte BYTE, int bits) {
+			return (byte) (BYTE << bits | ((BYTE & 0xff) >>> (8 - bits)));
+		}
+		
+		@Deprecated
+		private byte OFRightShift(byte BYTE, int bits) {
+			return (byte) (((BYTE & 0xff) >>> bits) | (BYTE << (8 - bits)));
 		}
 	}
+	
+	private static class Key extends Block {
+
+		private int round = 0;
+		
+		public Key(int round) {
+			super(new int[]{0x00000000, 0x00000000, 0x00000000, 0x00000000});
+			this.round = round;
+		}
+		
+		public Key(byte[] bytes) {
+			super(bytes);
+			this.round = 0;
+		}
+	
+	
+		public Key nextRoundKey() {
+			Key newRoundKey = new Key(round + 1);
+			int DWORD = getVDWORD(3);
+			DWORD = OFLeftShift(DWORD, 8);
+			DWORD = ByteArrayToDWORD(subBytes(DWORDtoByteArray(DWORD)));
+			newRoundKey.setVDWORD(0, (DWORD ^ getVDWORD(0)) ^ Key.getRcon(round));
+			for(int i = 0; i < 3; i++) 
+				newRoundKey.setVDWORD(i + 1, getVDWORD(i + 1) ^ newRoundKey.getVDWORD(i));
+			
+			return newRoundKey;
+		}
+		
+		private byte[] DWORDtoByteArray(int DWORD) {
+			byte[] b = new byte[4];
+			for(int i = 0; i < 4; i++)
+				b[i] = (byte) (DWORD >> (( 3 - i) * 8) & 0xFF);
+			return b;
+		}
+		
+		private int ByteArrayToDWORD(byte[] b) {
+			int DWORD = 0x00000000;
+			for(int i = 0; i < 4; i++)
+				DWORD |= (b[i] << (3 - i) * 8) & masks[i];
+			return DWORD;
+		}
+		
+		private static int getRcon(int i) {
+			return (((int) Math.pow(2, i) << 24)) & masks[0];
+		}
+		
+	}
+	
+	private static class EncryptionBlock extends Block {
+
+		public EncryptionBlock(byte[] bytes) {
+			super(bytes);
+		}
+		
+		public void shiftWords() {
+			shiftWord(1, 1);
+			shiftWord(2, 2);
+			shiftWord(3, 3);
+		}
+		
+	}
+	
+	
+	
 }
